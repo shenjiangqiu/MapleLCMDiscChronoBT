82a59fbf55ae1ba65c0e0565400fde0302e3ba83
Jiangqiu Shen <jshen2@mtu.edu>
2019-12-27 18:56:45 -0500

    fix some debug problem

--- sources/simp/depend.mk
+++ sources/simp/depend.mk
@@ -1,10 +1,10 @@
-/home/sjq/git/MapleLCMDiscChronoBT-DL-v3/sources/simp/SimpSolver.o /home/sjq/git/MapleLCMDiscChronoBT-DL-v3/sources/simp/SimpSolver.or /home/sjq/git/MapleLCMDiscChronoBT-DL-v3/sources/simp/SimpSolver.od /home/sjq/git/MapleLCMDiscChronoBT-DL-v3/sources/simp/SimpSolver.op: \
- /home/sjq/git/MapleLCMDiscChronoBT-DL-v3/sources/simp/SimpSolver.cc \
+/home/jshen/git/MapleLCMDiscChronoBT/sources/simp/SimpSolver.o /home/jshen/git/MapleLCMDiscChronoBT/sources/simp/SimpSolver.or /home/jshen/git/MapleLCMDiscChronoBT/sources/simp/SimpSolver.od /home/jshen/git/MapleLCMDiscChronoBT/sources/simp/SimpSolver.op: \
+ /home/jshen/git/MapleLCMDiscChronoBT/sources/simp/SimpSolver.cc \
  ../mtl/Sort.h ../mtl/Vec.h ../mtl/IntTypes.h ../mtl/XAlloc.h \
  ../simp/SimpSolver.h ../mtl/Queue.h ../core/Solver.h ../mtl/Heap.h \
  ../mtl/Alg.h ../utils/Options.h ../utils/ParseUtils.h \
  ../core/SolverTypes.h ../mtl/Map.h ../mtl/Alloc.h ../utils/System.h
-/home/sjq/git/MapleLCMDiscChronoBT-DL-v3/sources/simp/Main.o /home/sjq/git/MapleLCMDiscChronoBT-DL-v3/sources/simp/Main.or /home/sjq/git/MapleLCMDiscChronoBT-DL-v3/sources/simp/Main.od /home/sjq/git/MapleLCMDiscChronoBT-DL-v3/sources/simp/Main.op: /home/sjq/git/MapleLCMDiscChronoBT-DL-v3/sources/simp/Main.cc \
+/home/jshen/git/MapleLCMDiscChronoBT/sources/simp/Main.o /home/jshen/git/MapleLCMDiscChronoBT/sources/simp/Main.or /home/jshen/git/MapleLCMDiscChronoBT/sources/simp/Main.od /home/jshen/git/MapleLCMDiscChronoBT/sources/simp/Main.op: /home/jshen/git/MapleLCMDiscChronoBT/sources/simp/Main.cc \
  ../utils/System.h ../mtl/IntTypes.h ../utils/ParseUtils.h \
  ../utils/Options.h ../mtl/Vec.h ../mtl/XAlloc.h ../core/Dimacs.h \
  ../core/SolverTypes.h ../mtl/Alg.h ../mtl/Map.h ../mtl/Alloc.h \
